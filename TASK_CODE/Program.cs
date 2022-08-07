e21b72c0e8d0856ee099f64f4fa6641be03ca709
Shamil <karimshg@gmal.com>
2022-08-06 22:32:39 -0700

    add folder TASK_CODE

---
+++ TASK_CODE/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] array = {"hello", "2", "world", ":-)"};
+
+Console.Write("[");
+
+for (int i = 0; i < array.Length; i++)
+{
+    int length = array[i].Length;
+    if (length <= 3)
+    {
+        Console.Write(array [i] + " ");
+    }
+}
+Console.Write("]");
