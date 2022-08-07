a05e118badac25c31ea497d64290d4fd44d2524c
Shamil <karimshg@gmal.com>
2022-08-06 23:12:11 -0700

    изменения в  TASK_CODE

--- TASK_CODE/Program.cs
+++ TASK_CODE/Program.cs
@@ -9,7 +9,7 @@ for (int i = 0; i < array.Length; i++)
     int length = array[i].Length;
     if (length <= 3)
     {
-        Console.Write(array [i] + " ");
+        Console.Write(array [i] + ", ");
     }
 }
 Console.Write("]");
